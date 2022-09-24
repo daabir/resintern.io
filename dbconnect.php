838788ba35472f983fe259b2b07bb146b46f1af6
daabir <darmubashir2@gmail.com>
2022-09-24 20:01:53 +0530

    Website I built for internship

---
+++ dbconnect.php
@@ -0,0 +1,64 @@
+<?php
+
+
+/*                    // Database Connection
+                    $host = "localhost";
+                    $db_name = "resume";
+                    $username = "root";
+                    $pass = "";
+
+                    // connect to mysql
+                    $con = mysqli_connect($host,$username,$pass,$db_name);
+                    if (mysqli_connect_errno()){
+                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    die();
+                    }
+
+                    // for fetch basic details
+                    $sqlcommand ="SELECT * FROM basic";
+                    $result = mysqli_query($con,$sqlcommand);
+                    $rowdata = mysqli_fetch_array($result);
+
+					$url 		= 	$rowdata['url'];
+
+					$domain 	= 	"localhost";
+
+					$keyword 	= 	$rowdata['keyword'];
+
+					$sitename 	= 	$rowdata['sitename'];
+
+					$face 		= 	$rowdata['facebook'];
+
+					$insta 		= 	$rowdata['instagram'];
+
+					$twitter 	= 	$rowdata['twitter'];
+
+					$linkdin 	= 	$rowdata['linkden'];
+
+					$gmail		=   $rowdata['gmail'];
+
+*/
+
+
+
+
+                         $url           =    "https://localhost/resintern/";
+
+                         $domain   =    "localhost";
+
+                         $keyword  =    "resume, reusume buillder, resume maker";
+
+                         $sitename      =    "Easy Resume Build";
+
+                         $face          =    'https://facebook.com/daabir.mubashir';
+
+                         $insta         =    'https://instagram.com/daabir_mubashir';
+
+                         $twitter  =    'https://twitter.com/daabir_mubashir'; 
+
+                         $linkdin  =    'https://linkedin.com/in/mubashir-parwaz-dar-33247823a/';
+
+                         $gmail         =  'dardaabir2@gmail.com';
+
+
+?>
